5431d23023cb3fa1dd2e69b5e2ab4b032a4e0508
Navid Paya <navid@binary.com>
2014-06-04 08:53:23 +0000

    First version of the feed provider dealy plguin

---
+++ feed_provider_delay.rb
@@ -0,0 +1,17 @@
+# Author: Navid Paya <me@navidpaya.com>
+# This plguins checks creates a list of feed providers based on ymal
+# files and then find the lag for each of those providers and reports
+# it to scout.
+
+class FeedProviderDelapy < Scout::Plugin
+  needs 'json'
+
+  def build_report
+    provider_list = JSON.parse(`/usr/bin/bom_provider_delay.pm --list-used-providers`)
+    provider_list.each do |v|
+     provider_lag = `/usr/bin/bom_provider_delay.pm #{v}`
+         report(:provider => v , :lag => provider_lag)
+    end
+  end
+
+end
